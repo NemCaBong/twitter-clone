a58565c52edbb355942e0d125491fe292a2d936b
Nguyen Minh Hoang <nemcabong@gmail.com>
2024-01-15 13:10:12 +0700

    Tao tweetid validator

--- src/routes/bookmarks.routes.ts
+++ src/routes/bookmarks.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { bookmarkTweetController, unbookmarkTweetController } from '~/controller/bookmarks.controllers'
+import { tweetIdValidator } from '~/middlewares/tweets.middlewares'
 import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handlers'
 const bookmarksRouter = Router()
@@ -10,7 +11,13 @@ const bookmarksRouter = Router()
  * Header: { Authorization: Bearer <access_token> }
  * Body: { tweet_id: string }
  */
-bookmarksRouter.post('/', accessTokenValidator, verifiedUserValidator, wrapRequestHandler(bookmarkTweetController))
+bookmarksRouter.post(
+  '/',
+  accessTokenValidator,
+  verifiedUserValidator,
+  tweetIdValidator,
+  wrapRequestHandler(bookmarkTweetController)
+)
 
 /**
  * Description. Unbookmark a Tweet
@@ -22,6 +29,7 @@ bookmarksRouter.delete(
   '/tweets/:tweet_id',
   accessTokenValidator,
   verifiedUserValidator,
+  tweetIdValidator,
   wrapRequestHandler(unbookmarkTweetController)
 )
 
