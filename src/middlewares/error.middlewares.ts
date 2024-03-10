ab3e851653ccec528f703ab2d11e932ef888248c
NemCaBong <nemcabong@gmail.com>
2024-03-10 19:24:22 +0700

    Fix middleware sua loi

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -13,19 +13,30 @@ import { ErrorWithStatus } from '~/models/Errors'
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  // cả EntityError và ErrorWithStatus đều bắt vào đây
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR).json(omit(err, ['status']))
+  try {
+    // cả EntityError và ErrorWithStatus đều bắt vào đây
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR).json(omit(err, ['status']))
+    }
+    const finalErr: any = {}
+    // vì nếu throw new Error của JS => Sẽ có stack trace
+    // nên chúng ta phải chuyển enumerable của Error thành true
+    // rồi omit ['stack'] để ko trả về stack trace
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (!Object.getOwnPropertyDescriptor(err, key)?.enumerable || !Object.getOwnPropertyDescriptor(err, key)) {
+        return
+      }
+      finalErr[key] = err[key]
+      // Object.defineProperty(err, key, { enumerable: true })
+    })
+    // mặc định là lỗi server nếu là Error của JS
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: err.message,
+      error_info: omit(err, ['stack'])
+    })
+  } catch (error) {
+    res
+      .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+      .json({ message: 'Internal Server Error', errorInfo: omit(error as any, ['stack']) })
   }
-  // vì nếu throw new Error của JS => Sẽ có stack trace
-  // nên chúng ta phải chuyển enumerable của Error thành true
-  // rồi omit ['stack'] để ko trả về stack trace
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  // mặc định là lỗi server nếu là Error của JS
-  return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    error_info: omit(err, ['stack'])
-  })
 }
